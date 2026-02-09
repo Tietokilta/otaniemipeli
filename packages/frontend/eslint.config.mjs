9d2ca7e35af615627f0b26bf3104991bb7671f26
Petrus Asikainen <petrus.asikainen@tietokilta.fi>
2026-02-09 21:12:37 +0200

    fix: ignore buggy eslint rule

--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      // https://github.com/facebook/react/issues/34905
+      "react-hooks/set-state-in-effect": "off",
+    }
+  }
 ]);
 
 export default eslintConfig;
